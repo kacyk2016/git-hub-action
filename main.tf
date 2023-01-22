7b427dab91d1cb7979e971a3e425193fc2d93c5e
kacyk2016 <94801373+kacyk2016@users.noreply.github.com>
2023-01-22 09:29:27 -0600

    Update main.tf
    created main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,18 @@
+resource "aws_security_group" "example" {
+  name        = "example"
+  description = "Example security group"
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  security_groups = [aws_security_group.example.name]
+}
