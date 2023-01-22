0d6b0fbf2847fe092c535af3ef77196b462359e5
kacyk2016 <94801373+kacyk2016@users.noreply.github.com>
2023-01-22 09:56:08 -0600

    Update variables.tf
    added descriptions for all variables

--- variables.tf
+++ variables.tf
@@ -1,15 +1,23 @@
 variable "aws_region" {
-  default = "us-west-2"
+  description = "ohio_region"
+  type = string
+  default = "us-east-2"
 }
 
 variable "ami" {
+  description = "aws-ami"
+  type = string
   default = "ami-0ff8a91507f77f867"
 }
 
 variable "instance_type" {
+  description = "aws_instance"
+  type = string
   default = "t2.micro"
 }
 
 variable "key_name" {
+  description = "ec2_instance key"
+  type = string
   default = "my-key-pair"
 }
