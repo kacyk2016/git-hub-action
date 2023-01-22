1e00824b78f9d310a8ed17be4ee3c2be41999b4f
kacyk2016 <94801373+kacyk2016@users.noreply.github.com>
2023-01-22 09:39:04 -0600

    Update variables.tf
    updated my varible file with a new variable from the new job

--- variables.tf
+++ variables.tf
@@ -1,40 +1,15 @@
-variable "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 3.0"
-  default = "anne"
-  }
-
- variable                    = "my_ami" {
-  description = "aws region"
-  default = "us-east-2"
-  ami type = "ami-ebd02392"
-  }
-  
-varible "aws_instance_type" {          
- ami = "default"
- instance type "t2.micro"
-  monitoring             = true
-  }
-variable "key" {
- description "aws key"
- type = string
- default = "user1"
- }
- variable "vpc_security_group_ids" {
-  subnet_id              = "default"
-  }
-
-variable "web_server_sg" {
-  source = "terraform-aws-modules/security-group/aws//modules/http-80"
-  name        = "web-server"
-  }
-  
-variable = "Security group for web-server with HTTP ports open within VPC"
-  vpc_id      = "default"
-  }
-
- variable "ingress_cidr_blocks" {
-  type = string
-  default = []
+variable "aws_region" {
+  default = "us-west-2"
 }
 
+variable "ami" {
+  default = "ami-0ff8a91507f77f867"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  default = "my-key-pair"
+}
