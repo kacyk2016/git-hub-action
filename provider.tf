3af14cfab0ea91a3e63c02fbd14834c7174d26e4
kacyk2016 <94801373+kacyk2016@users.noreply.github.com>
2023-01-25 08:46:35 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,23 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
 provider "aws" {
-  region = "us-east-2"
+  version = "~> 4.0"
+  region  = "us-east-1"
 }
 
-terraform {
-  cloud {
-    organization = "FusionIT"
-
-    workspaces {
-      name = "git-hub-actionanne"
-    }
-  }
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
 }
