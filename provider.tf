fa5809a785ca5ccd648b2ada6fd39a5219e7cb42
kacyk2016 <94801373+kacyk2016@users.noreply.github.com>
2023-01-24 18:05:04 -0600

    Update provider.tf
    changed providers

--- provider.tf
+++ provider.tf
@@ -1,4 +1,21 @@
-provider "aes" {
-  project = "git-hub-actionk"
+# main.tf
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "2.70.0"
+    }
+  }
+}
+
+provider "aws" {
   region  = "us-east-2"
+}
+
+data "aws_iam_account_alias" "account" {
+}
+
+output "account" {
+  value = data.aws_iam_account_alias.account.account_alias
 }
