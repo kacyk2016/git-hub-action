4fd6e72b9832ab3c8f141f193fe825b9bed4f3c8
kacyk2016 <94801373+kacyk2016@users.noreply.github.com>
2023-01-24 18:11:01 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,21 +1,5 @@
-# main.tf
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "2.70.0"
-    }
-  }
-}
-
 provider "aws" {
-  region  = "us-east-2"
-}
-
-data "aws_iam_account_alias" "account" {
-}
-
-output "account" {
-  value = data.aws_iam_account_alias.account.account_alias
+  shared_credentials_file = "/Users/me/.aws/credentials"
+  profile                 = "default"
+  region                  = "us-east-2"
 }
