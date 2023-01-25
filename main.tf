554deb0271925f8c6bf7e7d9c508a4a5c204fdd2
kacyk2016 <94801373+kacyk2016@users.noreply.github.com>
2023-01-24 21:13:53 -0600

    Update main.tf
    changed main.tf file

--- main.tf
+++ main.tf
@@ -1,18 +1,48 @@
-resource "aws_security_group" "example" {
-  name        = "example"
-  description = "Example security group"
+
+
+
+
+
+resource "aws_security_group" "Hannah_security" {
+  name        = "Hannah_security"
+  description = "security group using terraform"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
-}
 
-resource "aws_instance" "example" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  security_groups = [aws_security_group.example.name]
-}
+ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "Hannah_security"
+  }
+}  
