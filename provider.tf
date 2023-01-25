85f8dad0f267861f87d6e71514f3ac3202a98adb
kacyk2016 <94801373+kacyk2016@users.noreply.github.com>
2023-01-24 18:29:47 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
 provider "aws" {
-  shared_credentials_files = "/Users/me/.aws/credentials"
-  profile                 = "default"
-  region                  = "us-east-2"
+  region = "us-east-2"
 }
