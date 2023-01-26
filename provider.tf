3e62b32438608e1a5d5f06abcb2ca8318453e779
kacyk2016 <94801373+kacyk2016@users.noreply.github.com>
2023-01-25 23:26:48 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,14 @@
-provider "aws" {
-  region     = "us-east-2"
-  access_key = "my-access-key"
-  secret_key = "my-secret-key"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
 }
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-2"
+}
+
