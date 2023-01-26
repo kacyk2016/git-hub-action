0bc73f30b98d3764fd58ae270421352beb15348a
kacyk2016 <94801373+kacyk2016@users.noreply.github.com>
2023-01-25 23:26:07 -0600

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,23 +1,22 @@
-variable "aws_region" {
-  description = "ohio_region"
-  type = string
-  default = "us-east-2"
-}
+variable "name_input" {
+    description = "instance name hannah"
+    type = string
+    default = "hannah"
+    }
 
-variable "ami" {
-  description = "aws-ami"
-  type = string
-  default = "ami-0d03b1ad793d7ac93"
-}
+variable "region" {
+    description = "the aws region"
+    type = string
+    default = "us-east-2"
+    }
 
-variable "instance_type" {
-  description = "aws_instance"
-  type = string
-  default = "t2.micro"
-}
-
-variable "key_name" {
-  description = "ec2_instance key"
-  type = string
-  default = "my-key-pair"
-}
+variable "my_ami" {
+    description = "aws ami"
+    type = string
+    default = "ami-0d03b1ad793d7ac93"
+    }
+variable "key" {
+    description = "aws key"
+    type = string
+    default = "newyear"
+    }
