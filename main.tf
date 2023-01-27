26c91fb9d719d6d6d58c949104acb135f1b873e5
kacyk2016 <94801373+kacyk2016@users.noreply.github.com>
2023-01-26 19:28:18 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
+module "tomcat" {
+source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+  name = var.name_input
 
+  ami                    = var.my_ami
+  instance_type          = "t2.micro"
+  key_name               = var.key
 
-
-
-
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
 resource "aws_security_group" "Hannah_security" {
   name        = "Hannah_security"
   description = "security group using terraform"
@@ -46,4 +55,3 @@ ingress {
     Name = "Hannah_security"
   }
 }  
-
